d6240e24186732d368bffe21143becf44c38f0d6
Jakub Mikulas <jakub@mikul.as>
2015-03-30 19:30:35 +0200

    Removed path from test

--- test/undefined_secretOrPublickey.tests.js
+++ test/undefined_secretOrPublickey.tests.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-var path = require('path');
 var jwt = require('../index');
 var JsonWebTokenError = require('../lib/JsonWebTokenError');
 var expect = require('chai').expect;
