3b02fc55faec677f941bc5e1df2a838342caa8a5
José Laya <jmlaya@gmail.com>
2015-07-23 13:46:45 -0430

    Add refresh token functionality

--- index.js
+++ index.js
@@ -197,3 +197,39 @@ JWT.verify = function(jwtString, secretOrPublicKey, options, callback) {
 
   return done(null, payload);
 };
+
+JWT.refresh = function (token, secretOrPrivateKey, options) {
+
+  var decodedToken, limitDate, payload;
+  decodedToken = undefined;
+
+  try {
+    decodedToken = jws.decode(token);
+  } catch (err) {
+    return new jwt.JsonWebTokenError("invalid token");
+  }
+
+  payload = decodedToken.payload;
+
+  if (!decodedToken)
+    return new jwt.JsonWebTokenError("invalid token");
+
+  if (typeof options === "undefined")
+    options = {};
+
+  if (typeof payload.exp !== "undefined") {
+
+    if (typeof options.toleranceDays === "undefined")
+      options.toleranceDays = 7;
+
+    limitDate = new Date(payload.exp * 1000);
+    limitDate.setDate(limitDate.getDate() + options.toleranceDays);
+
+    if (Math.floor(Date.now() / 1000) >= Math.floor(limitDate / 1000))
+      return new jwt.TokenExpiredError('jwt expired', new Date(payload.exp * 1000));
+    else
+      return jwt.sign(payload, process.env.TOKEN_SECRET || "oursecret", options);
+
+  } else
+    return token;
+};
