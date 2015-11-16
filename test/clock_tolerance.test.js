ec00d4716eca6bf0e00cdf90fa8637ba2ff5c0a6
Jacopo <jacopofar@users.noreply.github.com>
2015-11-16 22:36:42 +0100

    iat must be backdated, not in the future

--- test/clock_tolerance.test.js
+++ test/clock_tolerance.test.js
@@ -6,7 +6,7 @@ describe('clockTolerance', function() {
   it('should produce tokens slightly backdated', function () {
     var token = jwt.sign({foo: 123}, 'xxx', { expiresInMinutes: 5 , clockTolerance: 7 });
     var result = jwt.verify(token, 'xxx');
-    expect(result.iat).to.be.closeTo(Math.floor(Date.now() / 1000) + 7, 0.5);
+    expect(result.iat).to.be.closeTo(Math.floor(Date.now() / 1000) - 7, 0.5);
   });
   
    it('should not produce tokens slightly backdated if not requested', function () {
