efb4fda40af3e0a93af11351ad1bec0e9e8bf7ac
Josh Iverson <jiverson222@gmail.com>
2020-01-15 09:41:02 -0700

    fix: location

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   decode: require('./decode'),
   verify: require('./verify'),
   sign: require('./sign'),
-  timespan: require('./timespan'),
+  timespan: require('./lib/timespan'),
   JsonWebTokenError: require('./lib/JsonWebTokenError'),
   NotBeforeError: require('./lib/NotBeforeError'),
   TokenExpiredError: require('./lib/TokenExpiredError'),
