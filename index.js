203bc4cfd6ba992e4246679e996966793daa11f5
Josh Iverson <jiverson222@gmail.com>
2020-01-15 09:31:49 -0700

    feat: expose timespan util

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   decode: require('./decode'),
   verify: require('./verify'),
   sign: require('./sign'),
+  timespan: require('./timespan'),
   JsonWebTokenError: require('./lib/JsonWebTokenError'),
   NotBeforeError: require('./lib/NotBeforeError'),
   TokenExpiredError: require('./lib/TokenExpiredError'),
