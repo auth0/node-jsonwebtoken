57a80134f201b1329494dc948d64bf5726bddca0
Filip Skokan <panva.ip@gmail.com>
2020-09-04 22:05:00 +0200

    refactor: make decode non-enumerable
    see #741

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 module.exports = {
-  decode: require('./decode'),
   verify: require('./verify'),
   sign: require('./sign'),
   JsonWebTokenError: require('./lib/JsonWebTokenError'),
   NotBeforeError: require('./lib/NotBeforeError'),
   TokenExpiredError: require('./lib/TokenExpiredError'),
 };
+
+Object.defineProperty(module.exports, 'decode', {
+  enumerable: false,
+  value: require('./decode'),
+});
