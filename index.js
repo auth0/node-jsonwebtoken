a99fd4b473e257c2f50ff69c716db1c520bf9a78
Jake Lacey <jakewlacey@gmail.com>
2023-04-03 09:27:06 +0100

    fix(stubs): allow decode method to be stubbed (#876)
    * refactor: revert 15a1bc4 to to solve #875, #870, and possibly also #876

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 module.exports = {
+  decode: require('./decode'),
   verify: require('./verify'),
   sign: require('./sign'),
   JsonWebTokenError: require('./lib/JsonWebTokenError'),
   NotBeforeError: require('./lib/NotBeforeError'),
   TokenExpiredError: require('./lib/TokenExpiredError'),
 };
-
-Object.defineProperty(module.exports, 'decode', {
-  enumerable: false,
-  value: require('./decode'),
-});
