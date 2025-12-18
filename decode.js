a965cf71d8dff0d3a5c48c9c92ba991bfcfd8953
Boris Vasilev <boris.vasilev@soti.net>
2025-12-18 10:26:33 -0500

    Add documentation to functions

--- decode.js
+++ decode.js
@@ -1,5 +1,17 @@
 var jws = require('jws');
 
+/**
+ * (Synchronous) Returns the decoded payload without verifying if the signature is valid.
+ *
+ * > Warning: This will not verify whether the signature is valid. You should not use this for untrusted messages. You most likely want to use `jwt.verify` instead.
+ *
+ * > Warning: When the token comes from an untrusted source (e.g. user input or external request), the returned decoded payload should be treated like any other user input; please make sure to sanitize and only work with properties that are expected.
+ * @param {string} jwt The JsonWebToken string.
+ * @param {object?} options Options
+ * @param {object} options.json Force `JSON.parse` on the payload even if the header doesn't contain `"typ":"JWT"`.
+ * @param {boolean} options.complete Return an object with the decoded payload and header.
+ * @returns {{header: *, payload: string, signature: *}|string|null}
+ */
 module.exports = function (jwt, options) {
   options = options || {};
   var decoded = jws.decode(jwt, options);
